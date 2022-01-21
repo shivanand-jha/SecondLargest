75eb13a2d5499b89c27de01aa024328e86c4afba
shivanand-jha <shivanand.jha_cs20@gla.ac.in>
2022-01-21 09:21:27 +0530

    addition of an array

---
+++ src/addingMatrices.java
@@ -0,0 +1,36 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class addingMatrices {
+    public static void main(String[] args) {
+        int [][] a = new int[3][3];
+        int [][] b = new int[3][3];
+        int [][] c = new int[3][3];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the element of matrix A");
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                a[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println("enter the element of B");
+        for (int i = 0; i < b.length; i++) {
+            for (int j = 0; j < b[0].length; j++) {
+                b[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println("Result is : ");
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                c[i][j]=a[i][j]+b[i][j];
+            }
+        }
+        for (int[] x : c) {
+            for (int y : x) {
+                System.out.print(y + " ");
+            }
+            System.out.println();
+        }
+
+        }
+    }

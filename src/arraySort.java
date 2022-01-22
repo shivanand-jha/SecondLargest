79941bbc85f7dfeec6416d5aabf333c57aebb403
shivanand-jha <shivanand.jha_cs20@gla.ac.in>
2022-01-22 12:24:47 +0530

    String sort

---
+++ src/arraySort.java
@@ -0,0 +1,9 @@
+public class arraySort {
+    public static void main(String[] args) {
+        String[] arr ={"java","python","c ","jas"};
+        java.util.Arrays.sort(arr);
+        for (String a:arr) {
+            System.out.print(a+" ");
+        }
+    }
+}

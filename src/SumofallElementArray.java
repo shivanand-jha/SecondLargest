881f17519f5c793b8161129c4f67c6404e0008aa
shivanand-jha <shivanand.jha_cs20@gla.ac.in>
2022-01-23 15:13:37 +0530

    sum of all element of an array by variable argument

---
+++ src/SumofallElementArray.java
@@ -0,0 +1,16 @@
+public class SumofallElementArray {
+    static double sum (double ...x)
+    {
+        double s =0;
+        for (int i = 0; i < x.length; i++) {
+            s +=x[i];
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum());
+        System.out.println(sum(120.2,12.2));
+        System.out.println(sum(10,12,45,78,125));
+    }
+}
